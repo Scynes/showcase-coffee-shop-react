b2903969864f7a548707772a710786b15bb4fb01
Scynes <dustin.greyfield@gmail.com>
2023-09-14 01:47:00 -0400

    complete(SocialMedia): added responsive social media section component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import About from '@components/About';
 import Hero from '@components/Hero';
 import Navigation from '@components/Navigation';
+import SocialMedia from '@components/SocialMedia';
 import Stats from '@components/Stats';
 import { BrowserRouter } from 'react-router-dom';
 
@@ -12,6 +13,7 @@ const App = () => {
             <Hero />
             <About />
             <Stats />
+            <SocialMedia />
         </BrowserRouter>
     );
 }
---
+++ src/components/SocialMedia/SocialMedia.tsx
@@ -0,0 +1,21 @@
+const SocialMedia = () => {
+
+    return (
+        <section className="social-media section-wrapper flex column gap-4">
+            <h2>Follow Us</h2>
+            <p>Keep up-to-date with the latest news, offers, and events at our bustling downtown coffee shop.</p>
+            <div className="socials flex column gap-2 width-full">
+                <h3>Social</h3>
+                <p>Facebook</p>
+                <hr />
+                <p>Instagram</p>
+                <hr />
+                <p>Twitter</p>
+                <hr />
+                <p>LinkedIn</p>
+            </div>
+        </section>
+    );
+}
+
+export default SocialMedia;
