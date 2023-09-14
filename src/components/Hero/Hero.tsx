52a83cb147624147e349d09a725645cef8066806
Scynes <dustin.greyfield@gmail.com>
2023-09-14 03:40:32 -0400

    feat(Navigation): added autohiding background on scrolling

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,14 +1,8 @@
 const Hero = () => {
 
     return (
-        <section className='hero grid place-items-center relative section-wrapper'>
-            <div className="backdrop-container">
-                <div className='shape round-tr' />
-                <div className='shape round-br round-bl' />
-                <div className='shape round-tl' />
-                <div className='shape round-bl' />
-            </div>
-            <h1 className='absolute text-center'>Unleash your taste buds with our signature lattes.</h1>
+        <section className='hero section-wrapper grid place-items-center'>
+            <h1 className='text-center'>Brew-tiful</h1>
         </section>
     );
 }
--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,11 +1,23 @@
 import { NavLink } from 'react-router-dom';
 
 import Logo from '@assets/coffee-logo.svg';
+import { useEffect, useState } from 'react';
 
 const Navigation = () => {
 
+    const [ scrolling, setScrolling ] = useState(false);
+
+    const handleScroll = () => setScrolling(window.scrollY > 0);
+
+    useEffect(() => {
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, []);
+
     return (
-        <nav className='flex justify-space-between width-full'>
+        <nav className={ `flex justify-space-between width-full ${ scrolling ? 'scrolling' : '' }` }>
             <img className='logo' src={ Logo } alt="logo" />
             <NavLink to={ '/' }>Menu</NavLink>
             <NavLink to={ '/' }>About</NavLink>
